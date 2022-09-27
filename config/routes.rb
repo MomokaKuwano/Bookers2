0c83b46fb00bbc21d56df49edaf22633360c9754
EC2 Default User <ec2-user@ip-172-31-15-94.ap-northeast-1.compute.internal>
2022-09-27 12:37:53 +0000

    [user/show作成中]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new,:create,:index,:show]
+
+  resources :users, only: [:index,:show,:destroy]
+  resources :books, only: [:new,:create,:index,:show,:destroy]
   devise_for :users
   root to:'homes#top'
   get 'homes/about',as:'about'
